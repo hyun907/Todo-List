7b5afe0c63e973d85d6791270a373713e3253532
hyun907 <seunghyun020907@naver.com>
2024-06-19 02:54:24 +0900

    Feat: 더미데이터로 로그인 로직 구현

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,30 @@
 import React from "react";
+import { useState } from "react";
+
+const User = {
+  id: "test",
+  pw: "test123",
+};
 
 const Login = () => {
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+
+  const handleId = (e) => {
+    setId(e.target.value);
+  };
+  const handlePw = (e) => {
+    setPw(e.target.value);
+  };
+
+  const onClickConfirmButton = () => {
+    if (id === User.id && pw === User.pw) {
+      alert("로그인에 성공했습니다.");
+    } else {
+      alert("로그인에 실패했습니다.");
+    }
+  };
+
   return (
     <>
       <div className="loginPage">
@@ -9,24 +33,30 @@ const Login = () => {
           <div className="contentWrap">
             <div className="inputTitle">아이디</div>
             <div className="inputWrap">
-              <input className="input" placeholder="아이디를 입력하세요" />
-            </div>
-
-            <div className="errorMessageWrap">
-              올바른 아이디를 입력해주세요.
+              <input
+                className="input"
+                placeholder="아이디를 입력하세요"
+                value={id}
+                onChange={handleId}
+              />
             </div>
 
             <div className="inputTitle">비밀번호</div>
             <div className="inputWrap">
-              <input className="input" placeholder="비밀번호를 입력하세요"/>
-            </div>
-            <div className="errorMessageWrap">
-              올바른 비밀번호를 입력해주세요.
+              <input
+                type="password"
+                className="input"
+                placeholder="비밀번호를 입력하세요"
+                value={pw}
+                onChange={handlePw}
+              />
             </div>
           </div>
 
           <div className="btnWrap">
-            <button className="loginBtn">로그인</button>
+            <button onClick={onClickConfirmButton} className="loginBtn">
+              로그인
+            </button>
             <button className="registerBtn">회원가입</button>
           </div>
         </div>
