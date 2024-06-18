7c2123ce94ec42e578e59e1d397f991acda86e8e
hyun907 <seunghyun020907@naver.com>
2024-06-18 23:52:58 +0900

    Feat: 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Notfound from "./pages/Notfound";
 
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/home" element={<Home/>} />
+        <Route path="*" element={<Notfound/>} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
