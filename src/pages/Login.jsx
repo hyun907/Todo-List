c035fe473331228b210628f66a2f7817f1d47193
hyun907 <seunghyun020907@naver.com>
2024-06-19 02:18:06 +0900

    Feat: 로그인 페이지 1차 퍼블리싱

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <div className="loginPage">
+        <h1 className="logo">Plan T</h1>
+        <div className="login">
+          <div className="contentWrap">
+            <div className="inputTitle">아이디</div>
+            <div className="inputWrap">
+              <input className="input" placeholder="아이디를 입력하세요" />
+            </div>
+
+            <div className="errorMessageWrap">
+              올바른 아이디를 입력해주세요.
+            </div>
+
+            <div className="inputTitle">비밀번호</div>
+            <div className="inputWrap">
+              <input className="input" placeholder="비밀번호를 입력하세요"/>
+            </div>
+            <div className="errorMessageWrap">
+              올바른 비밀번호를 입력해주세요.
+            </div>
+          </div>
+
+          <div className="btnWrap">
+            <button className="loginBtn">로그인</button>
+            <button className="registerBtn">회원가입</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
